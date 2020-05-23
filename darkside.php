c6801ffdd47b658d02aac11575c2bffb8be532d2
[solenn] <solenn.massot@laplateforme.io>
2020-05-23 21:18:47 +0200

    //

---
+++ darkside.php
@@ -0,0 +1,216 @@
+<html>
+
+<head>
+    <title>Solenn Massot</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="short icon" href="images/moon/moon.svg">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <header>
+        <nav id="menu-sticky">
+            <a class="link-nav" href=#apropos>A propos de moi</a>
+            <a class="link-nav" href=#formation>Formations</a>
+            <a class="link-nav" href=#exp>Expériences</a>
+            <a class="link-nav" href=#comps>Compétences</a>
+            <a class="link-nav" href=#projet>Portfolio</a>
+            <img class="img-nav" src="images/moon/cristaux.png" />
+        </nav>
+    </header>
+    <main>
+        <article id="presentation">
+            <span id="t-pres">
+                <h1>Solenn Massot</h1><br />
+                Developpeuse web full-stack
+            </span>
+            <img id="img-pres" src="images/moon/cristal-et-lune.png" />
+        </article>
+        </article>
+        <nav id="menu-nonsticky">
+            <a class="link-nav" href=#apropos>A propos de moi</a>
+            <a class="link-nav" href=#formation>Formations</a>
+            <a class="link-nav" href=#exp>Expériences</a>
+            <a class="link-nav" href=#comps>Compétences</a>
+            <a class="link-nav" href=#projet>Portfolio</a>
+            <img class="img-nav" src="images/moon/cristaux.png" />
+        </nav>
+        <div id="background-apropos">
+        <div class="ancre" id="apropos"></div>
+        
+        <article id="a-propos">
+            <div id="img">
+            <img id="my" src="images/moon/myself.jpg" /><br />
+            </div>
+            <section class="section">
+                <span class="text">
+                    Je m'appelle Solenn, je suis actuellement en formation à La Plateforme_ pour acquérir des connaissances qui me permettront de devenir developpeuse web full stack.
+                    Forte d'une formation de 3 ans en graphisme, je suis capable de concevoir toute une charte graphique en plus de developper un site web.
+                    Je suis tombée dans le code un peu par accident, qui s'est finalement révelée être une passion. 
+                    Je suis créative, rigoureuse, dynamique et curieuse.
+                    Je recherche une alternance qui commencerait en septembre 2020.
+                    Vous pouvez consulter mon cv <a href="cv-solenn-massot.pdf" class="link-text" target = "_blank">ici</a>.
+
+                </span>
+            </section>
+        </article>
+        </div>
+        <div id="background-formation">
+        <div class="ancre" id="formation"></div>
+        <article id="formations">
+            <h1 id="titre-formation">Formations</h1><br />
+            <div id="div-formation">
+                <section class="d-formation">
+                    <h1 class="t-formation">Lycée Léonard De Vinci</h1>
+                    <span>
+                        <h2>2012 - 2015</h2><br />
+                        <h3>AMACV</h3>(Artisanat et métier d'art option communication visuelle)
+                        Prise en main des différents logiciel de la suite Adobe : InDesign, Illustrator, Photoshop<br />
+                        Formation en design graphique<br />
+                        Obtention du BEP AMACV<br />
+                    </span>
+                </section>
+                <section class="d-formation">
+                    <h1 class="t-formation">La Plateforme_</h1>
+                    <span>
+                        <h2>2019 - 2021</h2><br />
+                        <h3>Coding School</h3>
+                        Apprentissage de différents languages pour developper un site web : HTML, CSS, PHP, Javascript...<br />
+                        Des exemples de mes projets <a href="#projet">ici</a>
+                    </span>
+                </section>
+            </div>
+        </article>
+        </div>
+        <div id="background-experiences">
+        <div class="ancre" id="exp"></div>
+        <article id="experience">
+            <h1>Expériences</h1>
+            <section class="s-exp">
+                <span>
+                    <h2>2019 - Actuellement</h2>
+                    <h3>Secrétaire du bureau des élèves de La Plateforme_</h3><br />
+                    Organisation de différents évènements au sein de l'école, transmission nécessaire au bon fonctionnement de l'association...
+                </span>
+            </section>
+            <section class="s-exp">
+                <span>
+                    <h2>2017-2018</h2>
+                    <h3>Employée polyvalente en restauration rapide</h3>
+                    Préparation des commandes en adéquation avec la demande client, vente de produits additionnels
+                </span>
+            </section>
+            <section class="s-exp">
+                <span>
+                    <h2>2015-2016</h2>
+                    <h3>Service civique volontaire - Association Eurocircle</h3>
+                    Dans le cadre du programme Access en partenariat avec l'ambassade des Etats-Unis, volontariat auprès de différentes association et échange européen d'une durée de 10 jours en Roumanie
+                </span>
+            </section>
+            <section class="s-exp">
+                <span>
+                    <h2>2013 - 2015</h2>
+                    <h3>Stage dans le cadre des études au Lycée </h3>
+                    Stage dans différentes entreprise, conception et réalisation d'affiches, de flyers, de logo, pratique de la sérigraphie...
+                </span>
+            </section>
+        </article>
+        </div>
+        <div id="background-competences">
+        <div class="ancre" id="comps"></div>
+        <article id="competence">
+            <h1>Compétences</h1>
+            <section class="comp-d">
+                <div class="skillbar clearfix " data-percent="90%">
+                    <div class="skillbar-title"><span>HTML</span></div>
+                    <div class="skillbar-bar"></div>
+                    <div class="skill-bar-percent">90%</div>
+                </div>
+
+                <div class="skillbar clearfix " data-percent="85%">
+                    <div class="skillbar-title"><span>CSS3</span></div>
+                    <div class="skillbar-bar"></div>
+                    <div class="skill-bar-percent">85%</div>
+                </div>
+
+                <div class="skillbar clearfix " data-percent="60%">
+                    <div class="skillbar-title"><span>JavaScript</span></div>
+                    <div class="skillbar-bar"></div>
+                    <div class="skill-bar-percent">60%</div>
+                </div>
+
+                <div class="skillbar clearfix " data-percent="70%">
+                    <div class="skillbar-title"><span>PHP</span></div>
+                    <div class="skillbar-bar"></div>
+                    <div class="skill-bar-percent">70%</div>
+                </div>
+
+                <div class="skillbar clearfix " data-percent="40%">
+                    <div class="skillbar-title"><span>Symfony</span></div>
+                    <div class="skillbar-bar"></div>
+                    <div class="skill-bar-percent">40%</div>
+                </div>
+
+
+                <div class="skillbar clearfix " data-percent="75%">
+                    <div class="skillbar-title"><span>MySQL</span></div>
+                    <div class="skillbar-bar"></div>
+                    <div class="skill-bar-percent">75%</div>
+                </div>
+
+                <div class="skillbar clearfix " data-percent="80%">
+                    <div class="skillbar-title"><span>Photoshop</span></div>
+                    <div class="skillbar-bar"></div>
+                    <div class="skill-bar-percent">80%</div>
+                </div>
+
+                <div class="skillbar clearfix " data-percent="70%">
+                    <div class="skillbar-title"><span>Illustrator</span></div>
+                    <div class="skillbar-bar"></div>
+                    <div class="skill-bar-percent">70%</div>
+                </div>
+
+                <div class="skillbar clearfix " data-percent="75%">
+                    <div class="skillbar-title"><span>InDesign</span></div>
+                    <div class="skillbar-bar"></div>
+                    <div class="skill-bar-percent">75%</div>
+                </div>
+
+                <div class="skillbar clearfix " data-percent="80%">
+                    <div class="skillbar-title"><span>GitHub</span></div>
+                    <div class="skillbar-bar"></div>
+                    <div class="skill-bar-percent">80%</div>
+                </div>
+            </section>
+        </article>
+        </div>
+        <div id="background-projet">
+        <div class="ancre" id="projet"></div>
+        <article id="portefolio">
+            <section>
+                <h1 class="t-formation">Botanik</h1>
+                <img class="img-projet" src="images/moon/botanik.png">
+                <br/>
+                <span>En collaboration avec Mathilde Roussier, réalisé en HTML et CSS<br />
+                    Vous pouvez visiter le site <a href="botanique/accueil.html" class="link-text" target = "_blank">ici</a></span><br />
+                
+            </section>
+            <section>
+                <h1 class="t-formation">Livre d'or</h1>
+                <img class="img-projet" src="images/moon/livre-ot.png">
+</br>
+                <span>Réalisé en HTML, CSS et PHP<br />
+                    Vous pouvez visiter le site <a href="livre-or/index.php" class="link-text" target = "_blank">ici</a></span><br />
+                
+            </section>
+
+        </article>
+        </div>
+    </main>
+</body>
+<footer>
+    <span id="footer">Created with HTML, CSS, JS and a bit of magic by Solenn Massot, 2020</span>
+</footer>
+
+</html>
---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+      $(document).scroll(function(){
+          
+        if(window.scrollY >= 3000)
+        {
+            $('.skillbar').each(function(){
+                jQuery(this).find('.skillbar-bar').animate({
+                    width:jQuery(this).attr('data-percent')
+                },2000);
+            });
+        }
+      })
+})
+
